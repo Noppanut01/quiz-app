dfdf2db4265843a7786a2b0e84248e0339998c01
Noppanut <noppanut.jang2547@gmail.com>
2025-09-01 10:09:25 +0700

    quiz-app finished

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,13 @@
+import { useContext } from "react"
+import { DataContext } from "../App"
+
+
 function Menu() {
+    const { setAppState } = useContext(DataContext)
     return (
         <div className="menu" >
             <h1>Menu component</h1>
+            <button onClick={() => setAppState("quiz")}>เริ่มทำแบบทดสอบ</button>
         </div>
     )
 }
